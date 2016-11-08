cca198afeccf4a809e70a2952501bf3f3f5102c9
djgzhiyong <djgzhiyong@gmail.com>
2016-11-08 14:47:47 +0800

    new-

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def helloworld(request):
     return HttpResponse("<h1>Hello Django World... </h1>")
+
+
+def index(request):
+    return render(request, template_name='firstapp/index.html')
